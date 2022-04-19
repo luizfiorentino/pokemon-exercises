04e2cd9391afc22a80f811ae5f3b9ee1363ac59c
luizfiorentino <lgfiorentino@gmail.com>
2022-04-19 12:30:17 +0200

    mapping over props to display the characters

--- src/App.js
+++ src/App.js
@@ -3,53 +3,58 @@ import "./App.css";
 import Title from "./Components/Title";
 import PokeCard from "./Components/PokeCard";
 
+const all_pokemon = [
+  {
+    name: "Charizard",
+    weight: 90,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Blaze", "Solar power", "Tough claws", "Drought"],
+  },
+  {
+    name: "Bulbasaur",
+    weight: 6.9,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Overgrow", "Chlorophyll"],
+  },
+  {
+    name: "Mewtwo",
+    weight: 122,
+    awesome: true,
+    terrifying: true,
+    abilities: ["Pressure", "Unnerve", "Steadfast", "Insomnia"],
+  },
+  {
+    name: "Mega beedrill",
+    weight: 65,
+    awesome: false,
+    terrifying: true,
+    abilities: ["Intimidate", "Unnerve"],
+  },
+];
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Title title="Crazy Pokes" />
+      <main>
+        <Title title="Welcome to the Crazy Pokes" />
 
-        <PokeCard
-          name="Bulbassur"
-          weight={6.9}
-          awesome={true}
-          terrifying={false}
-          abilities={["Pressure", "Unnerve", "Steadfast", "Insomnia"]}
-        />
-
-        <PokeCard
-          name="Charizard"
-          weight={90}
-          awesome={true}
-          terrifying={false}
-          abilities={["Blaze", "Solar power", "Tough claws", "Drought"]}
-        />
-
-        <PokeCard
-          name="Mewtow"
-          weight={122}
-          awesome={true}
-          terrifying={true}
-          abilities={["Pressure", "Unnerve", "Steadfast claws", "Insomnia"]}
-        />
-
-        <PokeCard
-          name="Mega Beedrill"
-          weight={65}
-          awesome={false}
-          terrifying={true}
-          abilities={["Intimidate", "Unnerve power"]}
-        />
-      </header>
+        {all_pokemon.map((pokemon, i) => {
+          return (
+            <PokeCard
+              key={i}
+              name={pokemon.name}
+              weight={pokemon.weight}
+              awesome={pokemon.awesome}
+              terrifying={pokemon.terrifying}
+              abilities={pokemon.abilities}
+            />
+          );
+        })}
+      </main>
     </div>
   );
 }
 
 export default App;
-
-/*
-Charizard	90 kg	yes	no	Blaze, Solar power, Tough claws, Drought
-Bulbasaur	6.9 kg	yes	no	Overgrow, Chlorophyll
-Mewtwo	122 kg	yes	yes	Pressure, Unnerve, Steadfast, Insomnia
-Mega beedrill	65 kg	no	yes	Intimidate, Unnerve
-*/
